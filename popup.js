bcfaa70329ec1fc69a2339700eddf5f7b45909bf
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-08-27 19:38:28 +0900

    Performance: Reserve FFmpeg first

--- popup.js
+++ popup.js
@@ -1,13 +1,22 @@
 import "/libs/shazam-api.min.js";
 import { FFmpeg } from "/libs/ffmpeg/ffmpeg/dist/esm/index.js"
 
+let reservedFFmpeg = reserveFFmpeg()
 let audios = await getAudiosInTab()
 audios.forEach(async audio => {
-    let pcm = await convertToPCM(audios[0])
+    let pcm = await convertToPCM(audios[0], reservedFFmpeg)
     document.getElementById("result").insertAdjacentHTML("beforeend", `
         <h2>${JSON.stringify(await shazamGuess(pcm))}</h2>
     `)
 })
+
+function reserveFFmpeg(){
+    let ffmpeg = new FFmpeg();
+    let reserve = ffmpeg.load({
+        coreURL: "/libs/ffmpeg/core/dist/esm/ffmpeg-core.js",
+    })
+    return [ffmpeg, reserve]
+}
 
 async function getAudiosInTab(){
     let tabId = await chrome.tabs.query({active:true, currentWindow:true}).then(t => t[0].id)
@@ -28,21 +37,19 @@ async function sendMessagePromises(tabId){
 async function shazamGuess(pcm){
     let shazam = new Shazam.Shazam()
     let samples = Shazam.s16LEToSamplesArray(pcm);
-    let songData = await shazam.recognizeSong(samples);
-    return songData
+    return await shazam.recognizeSong(samples)
 }
 
-async function convertToPCM(audio){
-    let ffmpeg = new FFmpeg();
-    await ffmpeg.load({
-        coreURL: "/libs/ffmpeg/core/dist/esm/ffmpeg-core.js",
-    })
+async function convertToPCM(audio, reservedFFmpeg){
+    let [ffmpeg, reserve] = reservedFFmpeg
+    await reserve
     await ffmpeg.writeFile("audio.webm", new Uint8Array(audio));
     await ffmpeg.exec([
         "-i", "audio.webm",
         "-ar", "16000",
         "-ac", "1",
         "-f", "s16le",
+        "-y",
         "out.pcm"
     ])
     return await ffmpeg.readFile("out.pcm");

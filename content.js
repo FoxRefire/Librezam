378401216a1bf0589e8b7df7896937a8ea17bb35
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-08-28 13:32:29 +0900

    Fix for firefox

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     let elements = Array.from(document.querySelectorAll('audio, video')).filter(media => !media.paused)
     let promises = []
     elements.forEach(elem => {
-        let stream = elem.captureStream()
+        let stream = createStream(elem)
         let audioStream = new MediaStream(stream.getAudioTracks())
 
         let dataPromise = recordStream(audioStream, Number(request.time)).then(data => Array.from(data))
@@ -21,4 +21,15 @@ function recordStream(stream, ms){
         recorder.start()
         setTimeout(() => recorder.stop(), ms)
     })
+}
+
+function createStream(elem){
+    let stream = elem.captureStream ? elem.captureStream() : elem.mozCaptureStream()
+
+    if (!elem.audioCtx && !elem.captureStream){
+        elem.audioCtx = new AudioContext()
+        let source = elem.audioCtx.createMediaElementSource(elem)
+        source.connect(elem.audioCtx.destination)
+    }
+    return stream
 }

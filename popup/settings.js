eaeeb09bf17d77d4ced010e4913cd0326eb85004
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-10-08 22:41:25 +0900

    Specify the Recording Length using the input box instead of the slider

--- popup/settings.js
+++ popup/settings.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     let currentTime = await chrome.storage.local.get("time").then(o => o.time) || 3200
-    currentRecordLength.innerText = currentTime
     recordLength.value = currentTime
 })
 recordLength.addEventListener("change", async () => {
-    currentRecordLength.innerText = recordLength.value
     await chrome.storage.local.set({time: Number(recordLength.value)})
 })

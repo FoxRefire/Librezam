fd07e6574b811f73c019aff1c322887272609237
soulhotel <155501797+soulhotel@users.noreply.github.com>
2025-05-04 00:16:59 -0400

    background options
    automate user selection/application of background image, saved via storage api

--- popup/settings.js
+++ popup/settings.js
@@ -1,12 +1,31 @@
+// sync time
 document.addEventListener('DOMContentLoaded', async () => {
-    let currentTime = await chrome.storage.local.get("time").then(o => o.time) || 3200
-    recordLength.value = currentTime
-})
-recordLength.addEventListener("change", async () => {
-    await chrome.storage.local.set({time: Number(recordLength.value)})
-})
+    let currentTime = await chrome.storage.local.get("time").then(o => o.time) || 3200;
+    recordLength.value = currentTime;
+});
 
-M.Modal.init(modalConfirmClear, null)
+recordLength.addEventListener("change", async () => {
+    await chrome.storage.local.set({ time: Number(recordLength.value) });
+});
+
+M.Modal.init(modalConfirmClear, null);
 clearConfirmed.addEventListener("click", () => {
-    chrome.storage.local.set({histories: []})
-})
+    chrome.storage.local.set({ histories: [] });
+});
+
+// sync background
+document.addEventListener('DOMContentLoaded', async () => {
+    chrome.storage.local.get('selectedBackgroundImage', (data) => {
+        if (data.selectedBackgroundImage) {
+            document.body.style.backgroundImage = data.selectedBackgroundImage;
+        }
+    });
+
+    document.querySelectorAll('.bg-opt').forEach(opt => {
+        opt.addEventListener('click', () => {
+            const bgImage = opt.style.backgroundImage;
+            document.body.style.backgroundImage = bgImage;
+            chrome.storage.local.set({ selectedBackgroundImage: bgImage });
+        });
+    });
+});

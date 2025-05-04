fc711670676686d8ba0dbace0ee3332242fd5a3d
soulhotel <155501797+soulhotel@users.noreply.github.com>
2025-05-04 00:39:02 -0400

    background options (optimal approach)
    automate user selection/application of background image, saved via storage api

--- popup/settings.js
+++ popup/settings.js
@@ -13,19 +13,42 @@ clearConfirmed.addEventListener("click", () => {
     chrome.storage.local.set({ histories: [] });
 });
 
-// sync background
+// sync background by saving bg-opt-# (optimal)
 document.addEventListener('DOMContentLoaded', async () => {
-    chrome.storage.local.get('selectedBackgroundImage', (data) => {
-        if (data.selectedBackgroundImage) {
-            document.body.style.backgroundImage = data.selectedBackgroundImage;
+    chrome.storage.local.get('selectedBackgroundClass', (data) => {
+        if (data.selectedBackgroundClass) {
+            const savedOpt = document.querySelector(`.${data.selectedBackgroundClass}`);
+            if (savedOpt) {
+                document.body.style.backgroundImage = savedOpt.style.backgroundImage;
+            }
         }
     });
-
     document.querySelectorAll('.bg-opt').forEach(opt => {
         opt.addEventListener('click', () => {
-            const bgImage = opt.style.backgroundImage;
-            document.body.style.backgroundImage = bgImage;
-            chrome.storage.local.set({ selectedBackgroundImage: bgImage });
+            const bgClass = opt.classList.value.split(' ').find(cls => cls.startsWith('bg-opt-'));
+            if (bgClass) {
+                document.body.style.backgroundImage = opt.style.backgroundImage;
+                chrome.storage.local.set({ selectedBackgroundClass: bgClass });
+            }
         });
     });
 });
+
+// sync background by saving background style selected
+
+//document.addEventListener('DOMContentLoaded', async () => {
+    //chrome.storage.local.get('selectedBackgroundImage', (data) => {
+        //if (data.selectedBackgroundImage) {
+            //document.body.style.backgroundImage = data.selectedBackgroundImage;
+        //}
+    //});
+
+    //document.querySelectorAll('.bg-opt').forEach(opt => {
+        //opt.addEventListener('click', () => {
+            //const bgImage = opt.style.backgroundImage;
+            //document.body.style.backgroundImage = bgImage;
+            //chrome.storage.local.set({ selectedBackgroundImage: bgImage });
+        //});
+    //});
+//});
+

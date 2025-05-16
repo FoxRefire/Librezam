17f5db92cd27dc34e9041fadef1ec70f36d0d152
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2025-05-17 06:00:45 +0900

    Refactor code

--- popup/guess.js
+++ popup/guess.js
@@ -1,22 +1,16 @@
-import "/libs/shazam-api.min.js";
-import { FFmpeg } from "/libs/ffmpeg/ffmpeg/dist/esm/index.js"
+import { shazamGuess } from "/utils/shazamGuess.js"
 
 // grab background option from storage
 document.body.style.backgroundImage = await chrome.storage.local.get("bgImage").then(d => d.bgImage) || "url('/images/background-2.jpg')"
 
-
 writeHistory()
-let reservedFFmpeg = reserveFFmpeg()
-let audios = (await getAudiosInTab()).filter(a=> a.length)
 
+let audios = (await getAudiosInTab()).filter(a=> a.length)
 if(!audios.length){
     showError("No audio elements detected...")
 }
-
 audios.forEach(async audio => {
-    let pcm = await convertToPCM(audio, reservedFFmpeg)
-    let result = await shazamGuess(pcm)
-
+    let result = await shazamGuess(audio)
     if(result){
         writeResult(result)
         saveHistory(result)
@@ -31,19 +25,11 @@ async function writeHistory(){
         document.getElementById("historyTBody").insertAdjacentHTML("afterbegin",`
             <tr>
                 <td>${history.title}</td>
-                 <td>${history.artist}</td>
-                  <td>${history.year}</td>
+                <td>${history.artist}</td>
+                <td>${history.year}</td>
             </tr>
         `)
     })
-}
-
-function reserveFFmpeg(){
-    let ffmpeg = new FFmpeg();
-    let reserve = ffmpeg.load({
-        coreURL: "/libs/ffmpeg/core/dist/esm/ffmpeg-core.js",
-    })
-    return [ffmpeg, reserve]
 }
 
 async function getAudiosInTab(){
@@ -63,47 +49,27 @@ async function sendMessagePromises(tabId, ms){
     return promises
 }
 
-async function shazamGuess(pcm){
-    let shazam = new Shazam.Shazam()
-    let samples = Shazam.s16LEToSamplesArray(pcm);
-    return await shazam.fullRecognizeSong(samples)
-}
-
-async function convertToPCM(audio, reservedFFmpeg){
-    let [ffmpeg, reserve] = reservedFFmpeg
-    await reserve
-    await ffmpeg.writeFile("audio.webm", new Uint8Array(audio));
-    await ffmpeg.exec([
-        "-i", "audio.webm",
-        "-ar", "16000",
-        "-ac", "1",
-        "-f", "s16le",
-        "-y",
-        "out.pcm"
-    ])
-    return await ffmpeg.readFile("out.pcm");
-}
-
 function writeResult(result){
     circler.style.display = "none"
     resultTable.style.display = "block"
     streamProviders.style.display = "block"
 
-    titleResult.innerText = result.track.title
-    artistResult.innerText  = result.track.subtitle
-    yearResult.innerText = result.track.sections[0].metadata[2]?.text || ""
-
-    appleMusicLink.href = result.track.hub.options[0].actions[0].uri
-    deezerLink.href = result.track.hub.providers[1].actions[0].uri.replace("deezer-query://", "https://")
-    spotifyLink.href = "https://open.spotify.com/search/" + result.track.hub.providers[0].actions[0].uri.slice(15)
-    youtubeLink.href = "https://www.youtube.com/results?search_query=" + result.track.hub.providers[0].actions[0].uri.slice(15)
+    let elms = ["title", "artist", "year", "apple", "deezer", "spotify", "youtube"]
+    elms.forEach(out => {
+        let outElm = document.querySelector(`.result.${out}`)
+        if(!outElm.classList.contains("stream")) {
+            outElm.innerText = result[out]
+        } else {
+            outElm.href = result[out]
+        }
+    })
 }
 
 async function saveHistory(result){
     let newItem = {
-        title: result.track.title,
-        artist: result.track.subtitle,
-        year: result.track.sections[0].metadata[2]?.text || ""
+        title: result.title,
+        artist: result.artist,
+        year: result.year
     }
 
     let histories = await chrome.storage.local.get("histories").then(o => o.histories) || []
---
+++ utils/shazamGuess.js
@@ -0,0 +1,49 @@
+import "/libs/shazam-api.min.js"
+import { FFmpeg } from "/libs/ffmpeg/ffmpeg/dist/esm/index.js"
+
+let reservedFFmpeg = reserveFFmpeg()
+
+export async function shazamGuess(audio) {
+    let pcm = await convertToPCM(audio, reservedFFmpeg)
+    let response = await getResponse(pcm)
+    console.log(response)
+
+    return {
+        title: response.track.title,
+        artist: response.track.subtitle,
+        year: response.track.sections[0].metadata[2]?.text || "",
+        apple: response.track.hub.options[0].actions[0].uri,
+        deezer: response.track.hub.providers[1].actions[0].uri.replace("deezer-query://", "https://"),
+        spotify: "https://open.spotify.com/search/" + response.track.hub.providers[0].actions[0].uri.slice(15),
+        youtube: "https://www.youtube.com/results?search_query=" + response.track.hub.providers[0].actions[0].uri.slice(15)
+    }
+}
+
+function reserveFFmpeg() {
+    let ffmpeg = new FFmpeg();
+    let reserve = ffmpeg.load({
+        coreURL: "/libs/ffmpeg/core/dist/esm/ffmpeg-core.js",
+    })
+    return [ffmpeg, reserve]
+}
+
+async function convertToPCM(audio, reservedFFmpeg) {
+    let [ffmpeg, reserve] = reservedFFmpeg
+    await reserve
+    await ffmpeg.writeFile("audio.webm", new Uint8Array(audio));
+    await ffmpeg.exec([
+        "-i", "audio.webm",
+        "-ar", "16000",
+        "-ac", "1",
+        "-f", "s16le",
+        "-y",
+        "out.pcm"
+    ])
+    return await ffmpeg.readFile("out.pcm");
+}
+
+async function getResponse(pcm) {
+    let shazam = new Shazam.Shazam()
+    let samples = Shazam.s16LEToSamplesArray(pcm);
+    return await shazam.fullRecognizeSong(samples)
+}

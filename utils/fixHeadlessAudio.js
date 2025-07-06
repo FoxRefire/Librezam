ee5db6398414855f3844c9f8cce14192062cff3d
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2025-07-06 09:22:15 +0900

    Disable workaround script on osu.ppy.sh
    Closes #16

--- utils/fixHeadlessAudio.js
+++ utils/fixHeadlessAudio.js
@@ -2,12 +2,16 @@ let sitesNoDouble = [
     "soundcloud.com"
 ]
 
+let sitesNoAppend = [
+    "osu.ppy.sh"
+]
+
 let originalPlay = HTMLMediaElement.prototype.play
 HTMLMediaElement.prototype.play = function (...args) {
-    if(sitesNoDouble.includes(location.hostname)) {
+    if(testHost(sitesNoDouble)) {
         this.classList.add("librezamFlag")
     }
-    if(!isElemDOMAppended(this) && this.tagName == "AUDIO") {
+    if(!isElemDOMAppended(this) && this.tagName == "AUDIO" && !testHost(sitesNoAppend)) {
         document.body.append(this)
         console.log("Headless element appended", this)
     }
@@ -23,4 +27,8 @@ function isElemDOMAppended(elem){
     } else {
         return isElemDOMAppended(elem.parentNode)
     }
+}
+
+function testHost(hosts){
+    return hosts.includes(location.hostname)
 }
